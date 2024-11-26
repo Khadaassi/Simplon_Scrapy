3a46e651bd036ce80bab9d5c9fa9de2198201c56
Khadaassi <aassikhadija@yahoo.fr>
2024-11-26 13:43:02 +0100

    tout les fichiers pour avoir une base de travail

---
+++ venessens/venessens/spiders/testspider.py
@@ -0,0 +1,25 @@
+import scrapy
+
+
+class TestspiderSpider(scrapy.Spider):
+    name = "testspider"
+    allowed_domains = ["venessens-parquet.com"]
+    start_urls = ["https://venessens-parquet.com/collection/les-parquets-dinterieur/collection-archi/"]
+
+    def parse(self, response):
+        pages = response.css('li a::attr(href)').getall()
+
+        for page in pages:
+            if 'https://venessens-parquet.com/collections' in page:
+                yield response.follow(page, callback=self.parse_page)
+    
+    def parse_page(self, response):
+        reference = response.xpath('//span[@class="reference"]/text()').get()
+        nom = response.xpath('//h1/text()').get()
+        prix =response.xpath('//span[@class="prix"]/text()').get()
+        yield{
+            'Nom' : nom,
+            'Reference' : reference,
+            'Prix' : prix
+        }
+        
