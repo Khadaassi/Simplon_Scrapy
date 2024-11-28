782435aa3370c9ce0f16ce1e2fd73aefbab414ec
Khadaassi <aassikhadija@yahoo.fr>
2024-11-28 12:16:08 +0100

    next page et arborescence catégorie, + item produits

--- venessens/venessens/items.py
+++ venessens/venessens/items.py
@@ -1,12 +1,21 @@
 # Define here the models for your scraped items
 #
 # See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
+#https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
 
 
-class VenessensItem(scrapy.Item):
+class CategorieItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    nom_categorie_parent = scrapy.Field()
+    url_categorie_parent = scrapy.Field()
+    nom_categorie = scrapy.Field()
+    url_categorie = scrapy.Field()
+
+class ProduitItem(scrapy.Item):
+    nom_categorie = scrapy.Field()
+    nom_produit = scrapy.Field()
+    prix_produit = scrapy.Field()
+    reference_produit = scrapy.Field()
+    url_produit = scrapy.Field()
--- venessens/venessens/spiders/page_produit.py
+++ venessens/venessens/spiders/page_produit.py
@@ -1,47 +1,64 @@
 import scrapy
 import csv
+from venessens.items import ProduitItem
 
 
 class PageProduitSpider(scrapy.Spider):
     name = "page_produit"
     allowed_domains = ["venessens-parquet.com"]
-    start_url = ['https://venessens-parquet.com']
+    urls_depart = ['https://venessens-parquet.com']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.categories = self.read_csv()
+        self.categories = self.lire_csv()
 
     def start_requests(self):
         for categorie in self.categories:
             yield scrapy.Request(
-                url=categorie['URL'], 
-                callback= self.parse_page_produits,
-                meta= {'nom' : categorie['Catégorie']}
+                url=categorie['url_categorie'], 
+                callback=self.parse_pages_produits,
+                meta={'nom_categorie': categorie['nom_categorie']}
             )
 
-    def read_csv(self):
-        urls = []
-        with open('categories.csv', newline='') as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                urls.append({'Catégorie': row['Catégorie'], 'URL': row['URL']})
-        return urls
-    
-    def parse_page_produits(self, response):
-        pages = response.css('li a::attr(href)').getall()
-        for page in pages:
-            if 'https://venessens-parquet.com/collections' in page:
-                yield response.follow(page, callback=self.parse_produits, meta=response.meta)
-    
+    def lire_csv(self):
+        urls_categories = []
+        with open('categories.csv', newline='') as fichier_csv:
+            lecteur = csv.DictReader(fichier_csv)
+            for ligne in lecteur:
+                urls_categories.append({
+                    'nom_categorie': ligne['nom_categorie'], 
+                    'url_categorie': ligne['url_categorie']
+                })
+        return urls_categories
+
+    def parse_pages_produits(self, response):
+        liens_pages = response.css('li a::attr(href)').getall()
+        for lien_page in liens_pages:
+            if 'https://venessens-parquet.com/collections' in lien_page:
+                yield response.follow(lien_page, callback=self.parse_produits, meta=response.meta)
+        
+        lien_suivant = response.xpath('//a[@class="next page-numbers"]/@href').get()
+        if lien_suivant:
+            url_suivante = response.urljoin(lien_suivant)
+            yield response.follow(url=url_suivante, callback=self.parse_pages_produits, meta=response.meta)
+
     def parse_produits(self, response):
-        reference = response.xpath('//span[@class="reference"]/text()').get()
-        nom = response.xpath('//h1/text()').get()
-        prix =response.xpath('//span[@class="prix"]/text()').get()
-        produits = {
-            'Catégorie' : response.meta['nom'],
-            'Nom': nom.strip() if nom and nom.strip() else "Nom inconnu",
-            'Prix': prix.strip() if prix else "Prix non disponible",
-            'Reference': reference.strip() if reference else "Référence inconnue",
-            'URL' : response.url
-        }
-        yield produits
+        item = ProduitItem()
+        item['nom_categorie'] = response.meta['nom_categorie']
+        item['nom_produit'] = response.xpath('//h1/text()').get()
+        item['prix_produit'] = response.xpath('//span[@class="prix"]/text()').get()
+        item['reference_produit'] = response.xpath('//span[@class="reference"]/text()').get()
+        item['url_produit'] = response.url
+        yield item
+
+        # reference_produit = response.xpath('//span[@class="reference"]/text()').get()
+        # nom_produit = response.xpath('//h1/text()').get()
+        # prix_produit = response.xpath('//span[@class="prix"]/text()').get()
+
+        # yield {
+        #     'nom_categorie': response.meta['nom_categorie'],
+        #     'nom_produit': nom_produit.strip() if nom_produit and nom_produit.strip() else "Nom inconnu",
+        #     'prix_produit': prix_produit.strip() if prix_produit else "Prix non disponible",
+        #     'reference_produit': reference_produit.strip() if reference_produit else "Référence inconnue",
+        #     'url_produit': response.url
+        # }
