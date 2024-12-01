e810d037630bcbf6b20cd137de3af93acccc6168
Khadaassi <aassikhadija@yahoo.fr>
2024-12-01 19:29:42 +0100

    gestion des pipeline fonctionnelle.

--- venessens/venessens/pipelines.py
+++ venessens/venessens/pipelines.py
@@ -1,55 +1,60 @@
 import sqlite3
-from venessens.items import CategorieItem, ProduitItem
-from itemadapter import ItemAdapter
-import sqlite3
 
-
-class CategoriesPipeline:
-
+class VenessensPipeline:
     def open_spider(self, spider):
+        # Connexion à la base de données SQLite
         self.connection = sqlite3.connect("venessens.db")
         self.cursor = self.connection.cursor()
-        self.cursor.execute("""CREATE TABLE IF NOT EXISTS Categories(
-                            nom_categorie VARCHAR(100) PRIMARY KEY,
-                            url_categorie VARCHAR(500),
-                            is_page_list BOOLEAN
-                            ) """)
-        
+
+        # Création des tables si elles n'existent pas encore
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS categories (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nom_categorie TEXT NOT NULL,
+                url_categorie TEXT NOT NULL UNIQUE,
+                is_page_list BOOLEAN NOT NULL
+            )
+        ''')
+
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS produits (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nom_categorie TEXT NOT NULL,
+                nom_produit TEXT NOT NULL,
+                prix_produit TEXT,
+                reference_produit TEXT,
+                url_produit TEXT NOT NULL UNIQUE,
+                FOREIGN KEY (nom_categorie) REFERENCES categories(nom_categorie)
+            )
+        ''')
+        self.connection.commit()
+
     def process_item(self, item, spider):
-        self.cursor.execute("""INSERT OR IGNORE INTO Categories 
-                            (nom_categorie, url_categorie, is_page_list) 
-                            VALUES (?, ?, ?)""",
-                            (item['nom_categorie'], item['url_categorie'], item['is_page_list']))
+        if spider.name == "categorie":
+            # Insertion ou mise à jour des catégories
+            self.cursor.execute('''
+                INSERT OR IGNORE INTO categories (nom_categorie, url_categorie, is_page_list)
+                VALUES (?, ?, ?)
+            ''', (
+                item['nom_categorie'],
+                item['url_categorie'],
+                item['is_page_list']
+            ))
+        elif spider.name == "page_produit":
+            # Insertion ou mise à jour des produits
+            self.cursor.execute('''
+                INSERT OR IGNORE INTO produits (nom_categorie, nom_produit, prix_produit, reference_produit, url_produit)
+                VALUES (?, ?, ?, ?, ?)
+            ''', (
+                item['nom_categorie'],
+                item['nom_produit'],
+                item['prix_produit'],
+                item['reference_produit'],
+                item['url_produit']
+            ))
         self.connection.commit()
         return item
 
-
     def close_spider(self, spider):
+        # Fermeture de la connexion à la base de données
         self.connection.close()
-
-
-class DataBasePipeline :
-    def open_spider(self, spider):
-        self.connection = sqlite3.connect("venessens.db")
-        self.cursor = self.connection.cursor()
-        self.cursor.execute("""CREATE TABLE IF NOT EXISTS Produits(
-                            nom_categorie VARCHAR(50),
-                            nom_produit VARCHAR(100),
-                            prix_produit VARCHAR(20),
-                            reference_produit INT PRIMARY KEY,
-                            url_produit VARCHAR(255)
-                            ) """)
-        
-    def process_item(self, item, spider):
-        self.cursor.execute("""INSERT OR IGNORE INTO Produits 
-                            (nom_categorie, nom_produit, prix_produit, reference_produit, url_produit) 
-                            VALUES (?, ?, ?, ?, ?)""",
-                            (item['nom_categorie'], item['nom_produit'], item['prix_produit'], 
-                            item['reference_produit'], item['url_produit']))
-        self.connection.commit()
-        return item
-    
-    def close_spider(self, spider):
-        self.connection.close()
-
-
--- venessens/venessens/settings.py
+++ venessens/venessens/settings.py
@@ -63,10 +63,10 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-# ITEM_PIPELINES = {
-#    "venessens.pipelines.CategoriesPipeline" : 200,
-#    "venessens.pipelines.DataBasePipeline": 300
-# }
+ITEM_PIPELINES = {
+    'venessens.pipelines.VenessensPipeline': 300,
+}
+
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
